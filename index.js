b713ec28f625c3641981f12725bbad91d0d5b9f4
prashanth <prash488@gmail.com>
2021-06-26 13:42:00 +0800

    remove console log statement

--- index.js
+++ index.js
@@ -8,19 +8,18 @@ input.addEventListener('change', function (event) {
     reader.onload = function (event) {
         console.clear();
         var contents = event.target.result;
-        
+
         if (contents.indexOf('/Encrypt') !== -1) {
-           // console.log("Is encrypted");
+            // console.log("Is encrypted");
             button.disabled = true;
             alert('Uploading PDF files that are encrypted/password-protected is not allowed.')
         } else {
-           // console.log("Not encrypted");
+            // console.log("Not encrypted");
         }
-       console.log("File contents: " + contents);
     };
 
     reader.onerror = function (event) {
-        console.error("File could not be read! Code "  +event.target.error.code);
+        console.error("File could not be read! Code " + event.target.error.code);
     };
 
     reader.readAsText(input.files[0]);
